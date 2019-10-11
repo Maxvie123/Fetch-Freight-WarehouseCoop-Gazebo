3a6a1d854d94fb4f42d9ef4cf247a222427cbd86
Maxvie123 <jzl0196@auburn.edu>
2019-10-11 18:56:24 -0500

    change odometry source from simulated wheel encoder to gazebo. And map_server and amcl

--- script/robot_odom_pub.py
+++ script/robot_odom_pub.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import rospy
+import tf
 from nav_msgs.msg import Odometry
 from std_msgs.msg import Header
 from gazebo_msgs.srv import GetModelState, GetModelStateRequest
@@ -14,11 +15,15 @@ robot_name=rospy.get_param('robot_name')
 #publish the odom infomation in parent namespace
 odom_pub=rospy.Publisher ('odom', Odometry,queue_size=50)
 
+#create the tf broadcaster
+odom_broadcaster = tf.TransformBroadcaster()
+
 
 # wait for the 'gazebo/get_model_state' service launch
 rospy.wait_for_service ('/gazebo/get_model_state')
 get_model_srv = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
 
+#get the odometry from Gazebo
 odom=Odometry()
 header = Header()
 header.frame_id=robot_name +'/odom'
@@ -30,15 +35,37 @@ model.relative_entity_name='ground_plane'
 
 r = rospy.Rate(10)
 
+tf_parent = robot_name +'_tf/odom'
+tf_child = robot_name +'_tf/base_link'
+
 while not rospy.is_shutdown():
-    result = get_model_srv(model)
+	current_time = rospy.Time.now()
 
-    odom.pose.pose = result.pose
-    odom.twist.twist = result.twist
+	result = get_model_srv(model)
 
-    header.stamp = rospy.Time.now()
-    odom.header = header
+	tf_pose_x = result.pose.position.x
+	tf_pose_y = result.pose.position.y
+	tf_pose_z = result.pose.position.z
+	tf_head_x = result.pose.orientation.x
+	tf_head_y = result.pose.orientation.y
+	tf_head_z = result.pose.orientation.z
+	tf_head_w = result.pose.orientation.w
 
-    odom_pub.publish(odom)
+	tf_pose = (tf_pose_x, tf_pose_y, tf_pose_z)
+	tf_head = (tf_head_x, tf_head_y, tf_head_z, tf_head_w)
 
-    r.sleep()
+    #publish transform over tf
+	odom_broadcaster.sendTransform(tf_pose,tf_head,current_time,tf_child,tf_parent)
+
+    
+
+   #publish odometry message over ROS
+	odom.pose.pose = result.pose
+	odom.twist.twist = result.twist
+
+	header.stamp = rospy.Time.now()
+	odom.header = header
+
+	odom_pub.publish(odom)
+
+	r.sleep()
